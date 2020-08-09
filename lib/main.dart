a4e00e941e0b8064c28aa33aa2b12130c1604c62
kimchheng_heng <hchheng_178@yahoo.com>
2020-08-09 15:22:18 -0400

    shop app

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+        accentColor: Colors.limeAccent,
+
+      ),
+        home: Scaffold(
+          appBar: AppBar(title: Text("Shop app"),),
+          body: Center(child: Text("shop app"),),
+        ),
+//      routes: {}
+//      ,
+
+    );
+  }
+}
